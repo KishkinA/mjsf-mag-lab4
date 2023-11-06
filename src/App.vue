20bdba8b5d4b1c008a7f06de02a9eba5abcdb2dd
KishkinA <125496723+KishkinA@users.noreply.github.com>
2023-11-06 17:00:41 +0200

    Перший коміт

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TasksPage from './components/TasksPage.vue'
+</script>
+
+<template>
+  <section class="vh-100 vw-100 ">
+    <TasksPage/>
+  </section>
+</template>
+
+<style>
+#app {
+  margin: 0 !important;
+  padding: 0 !important;
+}
+</style>
+
